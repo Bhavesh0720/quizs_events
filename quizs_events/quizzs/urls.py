88ccf9dd3d2989b09258febba26f899aabd3a5d2
Bhavesh0720 <bhavesh0720@gmail.com>
2025-11-08 18:14:10 +0530

    added html pages and creates user authentication

--- quizs_events/quizzs/urls.py
+++ quizs_events/quizzs/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('events', views.events, name='events'),
+    path('quiz_list', views.quiz_list, name='quiz_list'),
+    path('quiz_attempt', views.quiz_attempt, name='quiz_attempt'),
+    path('result', views.result, name='result'),
 ]
--- quizs_events/quizzs/views.py
+++ quizs_events/quizzs/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        msg = ""
+        try:
+            User.objects.get(email=email)
+            msg = "this email exist use another!"
+        except User.DoesNotExist:
+            if password == confirm_password:
+                User.objects.create(
+                    username=username,
+                    email=email,
+                    password=password,
+                )
+                return redirect('signin')
+            else:
+                msg = "password and confirm password must match!"
+        con = {
+            'msg':msg,
+        }
+        return render(request, 'signup.html', con)
+    
+    return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        msg = ""
+        try:
+            user = User.objects.get(email=email)
+            if password == user.password:
+                request.session['user'] = user.id
+                return redirect('home')
+            else:
+                msg = "wrong password!"
+        except User.DoesNotExist:
+            msg = "this email is not registered!"
+        
+        con = {
+            'msg':msg
+        }
+        return render(request, 'signin.html', con)
+    
+    return render(request, 'signin.html')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def events(request):
+    return render(request, 'events.html')
+
+
+def quiz_list(request):
+    return render(request, 'quiz_list.html')
+
+
+def quiz_attempt(request):
+    return render(request, 'quiz_attempt.html')
+
+
+def result(request):
+    return render(request, 'result.html')
+
+
+def signout(request):
+    request.session.flush()
+    return redirect('signin')
